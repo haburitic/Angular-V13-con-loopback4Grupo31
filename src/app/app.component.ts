0854f9d2a302f67690fd13af2496771b2242aa00
Hector <Hector>
2021-12-01 20:29:39 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular-V13-con-loopback4';
+}
